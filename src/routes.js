7179c1a6dd201489f0ba1e9418305d844c635c12
AmbujaAK <itsambuja@gmail.com>
2020-05-24 00:33:33 +0530

    added event creation api and upload photo

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const multer = require('multer')
+
+const UserController = require('./controllers/UserController')
+const EventController = require('./controllers/EventController')
+const uploadConfig = require('./config/upload')
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
+//status
+routes.get('/', (req,res)=>{
+	res.send({ status: 200 })
+})
+
+//Event
+routes.post('/event', upload.single('thumbnail'), EventController.createEvent)
+routes.get('/event/:eventId', EventController.getEventById)
+
+
+//User
+routes.post('/user/createUser',UserController.createUser)
+routes.get('/user/:userId', UserController.getUserById)
+
+module.exports = routes
